7bb97ada3939b5a4b89ddfe2e03e05730e2155ca
diegors-prog <diegors@rede.ulbra.br>
2021-08-22 03:43:44 -0300

    refatorando o código

--- TDE_AULA3-EXERC-2/Agenda.cs
+++ TDE_AULA3-EXERC-2/Agenda.cs
@@ -1,9 +1,140 @@
+using System;
 using System.Collections.Generic;
 
 namespace TDE_AULA3_EXERC_2
 {
   public class Agenda
   {
-    public List<Contato> minhaAgenda = new List<Contato>();
+    List<Contato> listaDeContatos = new List<Contato>();
+
+    public void escrever(string valorDigitado)
+    {
+      Console.WriteLine(valorDigitado);
+    }
+
+    public string resposta() => Console.ReadLine();
+
+    void addContato()
+    {
+      string id = (listaDeContatos.Count + 1).ToString();
+      escrever("Digite o nome do contato: ");
+      string nome = resposta();
+      escrever("Digite o número do contato: ");
+      string numero = resposta();
+
+      Contato novoContato = new Contato(id, nome, numero);
+      listaDeContatos.Add(novoContato);
+    }
+
+    void editarContato()
+    {
+      escrever("Digite o id do usuário a ser editado");
+      listarContatos();
+      string contatoEditado = resposta();
+      foreach (Contato contato in listaDeContatos)
+      {
+        if (contato.id == contatoEditado)
+        {
+          escrever("Digite 1 para editar somente o nome");
+          escrever("Digite 2 para editar somente o número");
+          escrever("Digite 3 para editar o nome e o número");
+          string oper = resposta();
+          if (oper == "1")
+          {
+            escrever("Digite o novo nome");
+            string novoNome = resposta();
+            contato.setNome(novoNome);
+            escrever("O nome foi editado com sucesso!");
+            return;
+          }
+          else if (oper == "2")
+          {
+            escrever("Digite o novo número");
+            string novoNumero = resposta();
+            contato.setNumero(novoNumero);
+            escrever("O número foi editado com sucesso!");
+            return;
+          }
+          else if (oper == "3")
+          {
+            escrever("Digite o novo nome");
+            string novoNome = resposta();
+            contato.setNome(novoNome);
+            escrever("Agora digite o novo número");
+            string novoNumero = resposta();
+            contato.setNumero(novoNumero);
+            escrever("O nome e o número foram editados com sucesso!");
+            return;
+          }
+          else
+          {
+            escrever("Operação inválida");
+            return;
+          }
+        }
+      }
+    }
+
+    void excluirContato()
+    {
+      escrever("Digite o id do usuário a ser excluído");
+      listarContatos();
+      string contatoExcluido = resposta();
+      foreach (Contato contato in listaDeContatos)
+      {
+        if (contato.id == contatoExcluido)
+        {
+          listaDeContatos.Remove(contato);
+          escrever("Usuário excluído com sucesso!");
+          return;
+        }
+      }
+    }
+
+    void listarContatos()
+    {
+      foreach (Contato contato in listaDeContatos)
+      {
+        escrever(contato.getDescription());
+      }
+    }
+    public void menu()
+    {
+      string sair = "0";
+
+      do
+      {
+        escrever("Digite 1 para add um contato: ");
+        escrever("Digite 2 para editar um contato: ");
+        escrever("Digite 3 para excluir um contato: ");
+        escrever("Digite 4 para listar os contatos: ");
+        escrever("Digite Ctrl C para finalizar o programa");
+
+        string operador = resposta();
+
+        if (operador == "1")
+        {
+          addContato();
+        }
+        else if (operador == "2")
+        {
+          editarContato();
+        }
+        else if (operador == "3")
+        {
+          excluirContato();
+        }
+        else if (operador == "4")
+        {
+          listarContatos();
+        }
+        else
+        {
+          escrever("Opção invalida");
+        }
+
+      } while (sair != "99");
+
+    }
   }
 }
--- TDE_AULA3-EXERC-2/Contato.cs
+++ TDE_AULA3-EXERC-2/Contato.cs
@@ -2,13 +2,40 @@ namespace TDE_AULA3_EXERC_2
 {
   public class Contato
   {
-    public string nome { get; set; }
-    public string numero { get; set; }
+    public string id { get; set; }
+    string nome;
+    string numero;
 
-    public Contato(string nome, string num)
+    public Contato(string id, string nome, string numero)
+    {
+      this.id = id;
+      this.nome = nome;
+      this.numero = numero;
+    }
+
+    public string getNome()
+    {
+      return this.nome;
+    }
+
+    public void setNome(string nome)
     {
       this.nome = nome;
-      this.numero = num;
+    }
+
+    public string getNumero()
+    {
+      return this.numero;
+    }
+
+    public void setNumero(string numero)
+    {
+      this.numero = numero;
+    }
+
+    public string getDescription()
+    {
+      return "Id: " + id + " - " + nome + " -  Número: " + numero;
     }
   }
 }
